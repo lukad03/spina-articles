f00bef8702ae807cd428d52390ace42b8a048010
Luke Keller <lukeekeller@gmail.com>
2018-04-09 22:46:26 -0400

    Revert Install Generator to Previous State
    Reverts the install generator to its original state.
    
    * Revert install generator to original state

--- lib/generators/spina_articles/install_generator.rb
+++ lib/generators/spina_articles/install_generator.rb
@@ -1,8 +1,14 @@
+require 'rails/generators/base'
+require 'rails/generators/migration'
 require 'rails/generators/active_record'
+
 
 module SpinaArticles
   module Generators
-    class InstallGenerator < ActiveRecord::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
+
+      include Rails::Generators::Migration
+      extend ActiveRecord::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
 
@@ -11,18 +17,8 @@ module SpinaArticles
       end
 
       def copy_migration_file
-        migration_template "create_spina_articles_table.rb", Rails.root.join("db/migrate/create_spina_articles_table.rb"), migration_version: migration_version
-        migration_template "add_photo_id_to_spina_articles.rb", Rails.root.join("db/migrate/add_photo_id_to_spina_articles.rb"), migration_version: migration_version
-      end
-
-      def migration_version
-        if rails5?
-          "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
-        end
-      end
-
-      def rails5?
-        Rails.version.start_with? '5'
+        migration_template "create_spina_articles_table.rb", Rails.root.join("db/migrate/create_spina_articles_table.rb")
+        migration_template "add_photo_id_to_spina_articles.rb", Rails.root.join("db/migrate/add_photo_id_to_spina_articles.rb")
       end
     end
   end
